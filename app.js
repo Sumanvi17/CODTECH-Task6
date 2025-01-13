b11fdd3bf6496f3943f3d63335f3c417d0e5bbcf
sumanvi <163098300+Sumanvi17@users.noreply.github.com>
2025-01-13 13:17:51 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
+
+const App = () => (
+    <Router>
+        <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+    </Router>
+);
+
+export default App;
